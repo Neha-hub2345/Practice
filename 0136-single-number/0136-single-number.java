73f6546878d0c6139a6543139e31979b237cb680
Neha-hub2345 <kulkarniman03@gmail.com>
2026-07-06 19:22:57 +0530

    Time: 1 ms (99.97%), Space: 47.2 MB (7.12%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
-    public static int singleNumber(int[] nums){
-        for(int i = 0 ;i < nums.length; i++){
-            int frequency = 0;
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    frequency++;
-                }
-            }
-            if (frequency == 1) {
-            return nums[i];
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int num : nums){
+            result^=num;
         }
+        return result;
     }
-    return -1;
-}
 }
